75be1456c7db472f1680a641f745aeb2ef463d41
BulusuAvanthi <avu.bulusu@gmail.com>
2024-05-23 16:38:47 +0530

    Initial commit for Online Learning Platform

---
+++ OnlinelearningPlatform/src/test/java/com/project/OnlinelearningPlatform/OnlinelearningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.OnlinelearningPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinelearningPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
